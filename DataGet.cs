cacd24dc0a09d2d35b5d8f67a2577b5e9553d7f9
Connor Slade <turtleslade@gmail.com>
2021-01-24 22:22:30 -0500

    Base Working (Still needs Auto Update)

---
+++ DataGet.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace dataGet
+{
+    internal class DataGet
+    {
+        static public int getIndex(string path, string chat)
+        {
+            var text = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+            UTF8Encoding data = new UTF8Encoding(true);
+            Regex rg = new Regex(@"\[CHAT\] " + chat);
+            List<int> allInt = new List<int>();
+            byte[] b = new byte[64000];
+            string allText = "";
+
+            while (text.Read(b, 0, b.Length) > 0)
+            {
+                allText = data.GetString(b);
+                string[] textArray = allText.Split("\n");
+                foreach (string textIndex in textArray)
+                {
+                    Match match = rg.Match(textIndex);
+                    if (match.Success)
+                    {
+                        allInt.Add(int.Parse(textIndex.Split(chat)[1]));
+                    }
+                }
+            }
+            return allInt[allInt.Count - 1];
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using dataGet;
+
+namespace _2B2T_Queue_Notifier
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public Color BkHv = Color.FromRgb(67, 76, 94);
+        public Color BkLv = Color.FromRgb(45, 51, 63);
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+        #region TopBar
+        private void Rectangle_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+        }
+        private void Exit_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.Close();
+        }
+
+        private void Mini_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                WindowState = WindowState.Minimized;
+        }
+        #endregion
+        private void start_MouseEnter(object sender, MouseEventArgs e)
+        {
+            start.Fill = new SolidColorBrush(BkHv);
+        }
+
+        private void start_MouseLeave(object sender, MouseEventArgs e)
+        {
+            start.Fill = new SolidColorBrush(BkLv);
+        }
+        private void start_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            string path = Environment.ExpandEnvironmentVariables(@"%AppData%\.minecraft\logs\latest.log");
+            string chat = "Position in queue: ";
+            MainTime.Text = dataGet.DataGet.getIndex(path, chat).ToString();
+        }
+    }
+}
