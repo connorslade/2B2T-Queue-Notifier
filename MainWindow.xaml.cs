f2b2adc0a9d3e5440dbdbe78aacf5e001364034c
Connor Slade <turtleslade@gmail.com>
2021-01-24 22:27:54 -0500

    Auto Update Working

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,18 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using dataGet;
+using System.Windows.Threading;
 
 namespace _2B2T_Queue_Notifier
 {
@@ -21,19 +11,25 @@ namespace _2B2T_Queue_Notifier
     /// </summary>
     public partial class MainWindow : Window
     {
+        private DispatcherTimer dispatcherTimer;
         public Color BkHv = Color.FromRgb(67, 76, 94);
         public Color BkLv = Color.FromRgb(45, 51, 63);
+        private string path = Environment.ExpandEnvironmentVariables(@"%AppData%\.minecraft\logs\latest.log");
+        private string chat = "Position in queue: ";
+
         public MainWindow()
         {
             InitializeComponent();
-            
         }
+
         #region TopBar
+
         private void Rectangle_MouseDown(object sender, MouseButtonEventArgs e)
         {
             if (e.ChangedButton == MouseButton.Left)
                 this.DragMove();
         }
+
         private void Exit_MouseDown(object sender, MouseButtonEventArgs e)
         {
             if (e.ChangedButton == MouseButton.Left)
@@ -45,7 +41,9 @@ namespace _2B2T_Queue_Notifier
             if (e.ChangedButton == MouseButton.Left)
                 WindowState = WindowState.Minimized;
         }
-        #endregion
+
+        #endregion TopBar
+
         private void start_MouseEnter(object sender, MouseEventArgs e)
         {
             start.Fill = new SolidColorBrush(BkHv);
@@ -55,11 +53,23 @@ namespace _2B2T_Queue_Notifier
         {
             start.Fill = new SolidColorBrush(BkLv);
         }
+
         private void start_MouseDown(object sender, MouseButtonEventArgs e)
         {
-            string path = Environment.ExpandEnvironmentVariables(@"%AppData%\.minecraft\logs\latest.log");
-            string chat = "Position in queue: ";
+        }
+
+        private void dispatcherTimer_Tick(object sender, EventArgs e)
+        {
             MainTime.Text = dataGet.DataGet.getIndex(path, chat).ToString();
+        }
+
+        private void Grid_Initialized(object sender, EventArgs e)
+        {
+            MainTime.Text = dataGet.DataGet.getIndex(path, chat).ToString();
+            dispatcherTimer = new DispatcherTimer();
+            dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
+            dispatcherTimer.Start();
         }
     }
 }
