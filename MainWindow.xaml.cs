4fb7170c516baf882b124bdb9ef95f2aa2b7334d
Connor Slade <turtleslade@gmail.com>
2021-01-26 11:22:33 -0500

    Added Online and ... Modes

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
 using System.Windows;
 using System.Windows.Input;
 using System.Windows.Media;
@@ -20,10 +18,13 @@ namespace _2B2T_Queue_Notifier
         public Color TCL = Color.FromRgb(163, 190, 140);
         public Color TCM = Color.FromRgb(235, 203, 139);
         public Color TCF = Color.FromRgb(191, 97, 106);
+        public Color TCN = Color.FromRgb(94, 129, 172);
         private string webHook = "";
         private string path = Environment.ExpandEnvironmentVariables(@"%AppData%\.minecraft\logs\latest.log");
         private string chat = "Position in queue: ";
+        private int timeout = 30;
         private int indexCach;
+        private int EqFr;
 
         public MainWindow()
         {
@@ -63,6 +64,7 @@ namespace _2B2T_Queue_Notifier
                 this.Topmost = false;
             }
         }
+
         #endregion TopBar
 
         private void start_MouseEnter(object sender, MouseEventArgs e)
@@ -85,39 +87,62 @@ namespace _2B2T_Queue_Notifier
 
         private void dispatcherTimer_Tick(object sender, EventArgs e)
         {
-            List<int> FULL = dataGet.DataGet.getIndex(path, chat);
-            int index = FULL[0];
-            if (FULL[1] != indexCach)
+            try
             {
-                MainTime.Text = index.ToString();
-                if (index > 500)
+                List<int> FULL = dataGet.DataGet.getIndex(path, chat);
+                int lastChatEvent = dataGet.DataGet.ChatTime(path);
+                int index = FULL[0];
+                if (FULL[1] != indexCach && FULL[1] > dataGet.DataGet.NowTime() - timeout)
                 {
-                    MainTime.Foreground = new SolidColorBrush(TCF);
-                    dataGet.DataGet.discordWebHook(webHook, index.ToString(), indexCach, "12542314");
-                    indexCach = index;
+                    EqFr = 0;
+                    MainTime.Text = index.ToString();
+                    if (index > 500)
+                    {
+                        MainTime.Foreground = new SolidColorBrush(TCF);
+                        dataGet.DataGet.discordWebHook(webHook, index.ToString(), indexCach, "12542314");
+                        indexCach = index;
+                    }
+                    else if (index > 250 && index < 500)
+                    {
+                        MainTime.Foreground = new SolidColorBrush(TCM);
+                        dataGet.DataGet.discordWebHook(webHook, index.ToString(), indexCach, "15453067");
+                        indexCach = index;
+                    }
+                    else if (index > 0 && index < 250)
+                    {
+                        MainTime.Foreground = new SolidColorBrush(TCL);
+                        dataGet.DataGet.discordWebHook(webHook, index.ToString(), indexCach, "10731148");
+                        indexCach = index;
+                    }
+                    indexCach = FULL[1];
                 }
-                else if (index > 250 && index < 500)
+                else if (FULL[1] != lastChatEvent && lastChatEvent > dataGet.DataGet.NowTime() - timeout)
                 {
-                    MainTime.Foreground = new SolidColorBrush(TCM);
-                    dataGet.DataGet.discordWebHook(webHook, index.ToString(), indexCach, "15453067");
-                    indexCach = index;
-                }
-                else if (index > 0 && index < 250)
-                {
+                    MainTime.Text = "Online!";
                     MainTime.Foreground = new SolidColorBrush(TCL);
-                    dataGet.DataGet.discordWebHook(webHook, index.ToString(), indexCach, "10731148");
-                    indexCach = index;
                 }
-                indexCach = FULL[1];
+                else
+                {
+                    EqFr++;
+                    if (EqFr > timeout)
+                    {
+                        EqFr = 0;
+                        MainTime.Text = "…";
+                        MainTime.Foreground = new SolidColorBrush(TCF);
+                    }
+                }
+
             }
+            catch { MainTime.Text = "…"; }
         }
 
         private void Grid_Initialized(object sender, EventArgs e)
         {
-            MainTime.Text = dataGet.DataGet.getIndex(path, chat).ToString();
+            //MainTime.Text = dataGet.DataGet.getIndex(path, chat)[0].ToString();
+            MainTime.Text = "…";
             dispatcherTimer = new DispatcherTimer();
             dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
-            dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 1); //1-5?
             dispatcherTimer.Start();
         }
     }
