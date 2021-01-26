b8d101b46b8def52bf052b0c6b2c460b36119e13
Connor Slade <turtleslade@gmail.com>
2021-01-26 11:22:12 -0500

    Get ChatTime and Current Time

--- DataGet.cs
+++ DataGet.cs
@@ -16,7 +16,7 @@ namespace dataGet
             UTF8Encoding data = new UTF8Encoding(true);
             Regex rg = new Regex(@"\[CHAT\] " + chat);
             List<int> allInt = new List<int>();
-            byte[] b = new byte[64000];
+            byte[] b = new byte[640000];
             string allText = "";
             List<int> allTimeList = new List<int>();
             int totalTime;
@@ -41,15 +41,61 @@ namespace dataGet
                 }
             }
             List<int> Output = new List<int>();
-            Output.Add(allInt[allInt.Count - 1]);
-            Output.Add(allTimeList[allTimeList.Count - 1]);;
+            try
+            {
+                Output.Add(allInt[allInt.Count - 1]);
+                Output.Add(allTimeList[allTimeList.Count - 1]);
+            } catch { }
             return Output;
+        }
+
+        static public int ChatTime(string path)
+        {
+            var text = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+            UTF8Encoding data = new UTF8Encoding(true);
+            Regex rg = new Regex(@"\[CHAT\] ");
+            byte[] b = new byte[640000];
+            string allText = "";
+            int totalTime = 0;
+
+            while (text.Read(b, 0, b.Length) > 0)
+            {
+                allText = data.GetString(b);
+                string[] textArray = allText.Split("\n");
+                foreach (string textIndex in textArray)
+                {
+                    Match match = rg.Match(textIndex);
+                    if (match.Success)
+                    {
+                        totalTime = 0;
+                        string[] textSub = textIndex.Substring(1, 8).Split(':');
+                        totalTime += int.Parse(textSub[0]) * 60 * 60;
+                        totalTime += int.Parse(textSub[1]) * 60;
+                        totalTime += int.Parse(textSub[2]);
+                    }
+                }
+            }
+            return totalTime;
+        }
+
+        static public int NowTime()
+        {
+            string S = DateTime.Now.ToString("ss");
+            string M = DateTime.Now.ToString("mm");
+            string H = DateTime.Now.ToString("HH");
+            int totalTime = 0;
+            totalTime += int.Parse(H) * 60 * 60;
+            totalTime += int.Parse(M) * 60;
+            totalTime += int.Parse(S);
+          
+            return totalTime;
         }
 
         static public void discordWebHook(String WebHook, String Queue, int Cach, String Color)
         {
             if (int.Parse(Queue) != Cach)
             {
+                string time = DateTime.Now.ToString("HH:mm:ss • MM/dd/yy");
                 var httpWebRequest = (HttpWebRequest)WebRequest.Create(WebHook);
                 httpWebRequest.ContentType = "application/json";
                 httpWebRequest.Method = "POST";
@@ -58,9 +104,7 @@ namespace dataGet
                 {
                     string json = "{\"content\":null,\"embeds\":[{\"title\":\"Σ's 2B2T Queue Notifier\",\"description\":\"**Queue Posision:** `" + Queue.ToString()
                         + "`\",\"url\":\"https://github.com/Basicprogrammer10/2B2T-Queue-Notifier\",\"color\":"+Color
-                        +",\"footer\": {\"text\": \"10:49 • 01/25/2021\"},\"thumbnail\":{\"url\":\"https://i.imgur.com/K1KWFjR.png\"}}]}";
-                    MessageBox.Show(json);
-
+                        +",\"footer\": {\"text\": \""+time+"\"},\"thumbnail\":{\"url\":\"https://i.imgur.com/K1KWFjR.png\"}}]}";
                     streamWriter.Write(json);
                 }
 
