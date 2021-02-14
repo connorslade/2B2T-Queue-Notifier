a0e9cca0e9befbdd27d395c96ce6720e778439a8
Connor Slade <turtleslade@gmail.com>
2021-02-14 15:56:20 -0500

    Move INI config Reading Class to New File

--- src/DataGet.cs
+++ src/DataGet.cs
@@ -2,8 +2,6 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Net;
-using System.Reflection;
-using System.Runtime.InteropServices;
 using System.Text;
 using System.Text.RegularExpressions;
 
@@ -169,41 +167,6 @@ namespace _2B2T_Queue_Notifier
                 }
                 return true;
             } catch { return false; }
-        }
-    }
-
-
-    internal class IniFile
-    {
-        private readonly string Exe = Assembly.GetExecutingAssembly().GetName().Name;
-        private readonly string Path;
-
-        public IniFile(string IniPath = null)
-        {
-            Path = new FileInfo(IniPath ?? Exe + ".ini").FullName;
-        }
-
-        [DllImport("kernel32", CharSet = CharSet.Unicode)]
-        private static extern long WritePrivateProfileString(string Section, string Key, string Value, string FilePath);
-
-        [DllImport("kernel32", CharSet = CharSet.Unicode)]
-        private static extern int GetPrivateProfileString(string Section, string Key, string Default, StringBuilder RetVal, int Size, string FilePath);
-
-        public string Read(string Key, string Section = null)
-        {
-            var RetVal = new StringBuilder(255);
-            GetPrivateProfileString(Section ?? Exe, Key, "", RetVal, 255, Path);
-            return RetVal.ToString();
-        }
-
-        public void Write(string Key, string Value, string Section = null)
-        {
-            WritePrivateProfileString(Section ?? Exe, Key, Value, Path);
-        }
-
-        public bool KeyExists(string Key, string Section = null)
-        {
-            return Read(Key, Section).Length > 0;
         }
     }
 }
---
+++ src/IniFile.cs
@@ -0,0 +1,41 @@
+﻿using System.IO;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace _2B2T_Queue_Notifier
+{
+    internal class IniFile
+    {
+        private readonly string Exe = Assembly.GetExecutingAssembly().GetName().Name;
+        private readonly string Path;
+
+        public IniFile(string IniPath = null)
+        {
+            Path = new FileInfo(IniPath ?? Exe + ".ini").FullName;
+        }
+
+        [DllImport("kernel32", CharSet = CharSet.Unicode)]
+        private static extern long WritePrivateProfileString(string Section, string Key, string Value, string FilePath);
+
+        [DllImport("kernel32", CharSet = CharSet.Unicode)]
+        private static extern int GetPrivateProfileString(string Section, string Key, string Default, StringBuilder RetVal, int Size, string FilePath);
+
+        public string Read(string Key, string Section = null)
+        {
+            var RetVal = new StringBuilder(255);
+            GetPrivateProfileString(Section ?? Exe, Key, "", RetVal, 255, Path);
+            return RetVal.ToString();
+        }
+
+        public void Write(string Key, string Value, string Section = null)
+        {
+            WritePrivateProfileString(Section ?? Exe, Key, Value, Path);
+        }
+
+        public bool KeyExists(string Key, string Section = null)
+        {
+            return Read(Key, Section).Length > 0;
+        }
+    }
+}
