8607592540cf7b25c11acf6c3dc98b91b809e3c5
Connor Slade <connor@connorcode.com>
2021-10-09 13:12:50 -0400

    🎀 Auto Updateing Working

--- new/src/common.rs
+++ new/src/common.rs
@@ -1,1 +1,14 @@
+use iced::Color;
+
+pub fn update_color(pos: u32) -> Color {
+    if pos >= 500 {
+        return Color::from_rgb8(191, 97, 106);
+    }
+
+    if pos >= 250 {
+        return Color::from_rgb8(235, 203, 139);
+    }
+
+    Color::from_rgb8(163, 190, 140)
+}
 
